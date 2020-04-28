40ed9cbaf9cef60689101f71e8d577d31b005122
jkw <kyalo90@gmail.com>
2020-04-28 22:39:52 +0300

    updates.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -21,6 +21,8 @@ def topics(request):
 def topic(request, topic_id):
 	# show a single topic and all its entries
 	topic = Topic.objects.get(id=topic_id)
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
@@ -35,7 +37,9 @@ def new_topic(request):
 		# POST data submitted, process data
 		form = TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 
 	context = {'form': form}
@@ -65,6 +69,8 @@ def edit_entry(request, entry_id):
 	# add new entry for a particular topic
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 
 	if request.method != 'POST':
 		# initial request, pre-fill form with current entry
